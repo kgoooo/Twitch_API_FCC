6bff423af8e4e16f97ded566c9168541c47048d8
Kristen Goehringer <kgoehringer@gmail.com>
2017-08-24 10:48:49 +0200

    full functionality, pre refactor

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-//  https://api.twitch.tv/kraken/streams/esl_sc2?client_id=qc6mfya171mgkww1dn0s5p3wu3hq59
-
-const users = ["ogn_ow", "freecodecamp","esl_sc2", "noobs2ninjas", "c9shroud", "jasonr", "cretetion"]
+const users = ["ogn_ow", "freecodecamp","esl_sc2", "leh_tv", "lachu", "jasonr", "cretetion"]
 // const streamUrl = `https://api.twitch.tv/kraken/streams/${user}?client_id=${clientID}`
 // const userUrl = `https://wind-bow.glitch.me/twitch-api/users/${user}`
-
+let state = 'all';
 $(document).ready(() => {
-	users.forEach((user) => {
-		userGet(user);
-		streamGet(user);
-	});
+	fetchAll();
+	state = 'all'
 });
+
+const fetchAll = (online) => {
+	users.forEach((user) => {
+		userGet(user, online);
+	});
+}
 
 const userGet = (user) => {
 	$.ajax({
@@ -18,38 +20,125 @@ const userGet = (user) => {
 		dataType: "jsonp",
 		format:'json',
 		success: (userData) =>{
-			generateResult(userData);
+			streamGet(user, userData);
 		}
 	});
 }
 
-const streamGet = (user) =>{
+const streamGet = (user, userData) =>{
 	$.ajax({
 		type: 'GET',
 		url: `https://wind-bow.gomix.me/twitch-api/streams/${user}`,
 		dataType: "jsonp",
 		format:'json',
 		success: (streamData) =>{
-			console.log(streamData);
+			generateResult(userData, streamData)
 		}
 	});
 }
 
 const generateResult = (userData, streamData) => {
-	$("#insert").append(
-		`<div class="result-box grid"><div class="content-box col-1">
-			<img class="avatar" src="${userData.logo}" alt="avatar">
-		</div>				
-		<div class="avatar-nest col-7">
-			<h3 id="user-name">${userData.display_name}</h3>
-			<p>Preview</p></p>
-		</div>
-			<div class="status-box col-4">
-				<h2 id="stream-status">LIVE!</h2>
-				<img id="stream-preview" class="preview" src="https://static-cdn.jtvnw.net/previews-ttv/live_user_esl_sc2-320x180.jpg">
+	
+	if (state == 'all' && streamData.stream) {
+		$("#insert").append(
+				`<a href="https://www.twitch.tv/${userData.display_name}"><div class="result-box grid"><div class="content-box col-1">
+				<img class="avatar" src="${userData.logo}" alt="avatar">
+			</div>				
+			<div class="avatar-nest col-7">
+				<h3 id="user-name">${userData.display_name}</h3>
+				<p>Playing: ${streamData.stream.game}</p>
+				<p>${streamData.stream.channel.status}</p>
 			</div>
-		</div>`
-	);
+				<div class="status-box col-3">
+				<h2 id="stream-status">${streamData.stream.stream_type}</h2>
+				<img id="stream-preview" class="preview" src=${streamData.stream.preview.medium}>
+			</div>
+		</div>
+		</a>`
+		)
+	} if (state == 'all' && !streamData.stream) {
+			$("#insert").append(
+				`<div class="result-box grid"><div class="content-box col-1">
+					<img class="avatar" src="${userData.logo}" alt="avatar">
+				</div>				
+				<div class="avatar-nest col-7">
+					<h3 id="user-name">${userData.display_name}</h3>
+				</div>
+			<div class="status-box col-3">
+					<h2 id="stream-status">offline</h2>
+					<i class="fa fa-bed"></i>
+				</div>
+			</div>`
+		)
+	} if (state == 'online' && streamData.stream) {
+			$("#insert").append(
+					`<a href="https://www.twitch.tv/${userData.display_name}"><div class="result-box grid"><div class="content-box col-1">
+					<img class="avatar" src="${userData.logo}" alt="avatar">
+				</div>				
+				<div class="avatar-nest col-7">
+					<h3 id="user-name">${userData.display_name}</h3>
+					<p>Playing: ${streamData.stream.game}</p>
+					<p>${streamData.stream.channel.status}</p>
+				</div>
+					<div class="status-box col-3">
+					<h2 id="stream-status">${streamData.stream.stream_type}</h2>
+					<img id="stream-preview" class="preview" src=${streamData.stream.preview.medium}>
+				</div>
+			</div>
+			</a>`
+		)
+	} if (state == 'offline' && !streamData.stream){
+			$("#insert").append(
+				`<div class="result-box grid"><div class="content-box col-1">
+					<img class="avatar" src="${userData.logo}" alt="avatar">
+				</div>				
+				<div class="avatar-nest col-7">
+					<h3 id="user-name">${userData.display_name}</h3>
+				</div>
+			<div class="status-box col-3">
+					<h2 id="stream-status">offline</h2>
+					<i class="fa fa-bed"></i>
+				</div>
+			</div>`
+			)
+		}
 }
 
+const clearAll = () =>{
+	$("#insert").empty();
+}
 
+const reload = () => {
+	clearAll();
+	fetchAll();
+}
+
+$("#all-ch").on('click', () => {
+	state = 'all'			
+	reload()
+});
+
+$("#online-ch").on('click', () => {
+	state = 'online'
+	reload()
+});
+
+$("#offline-ch").on('click', () => {
+	state = 'offline'
+	reload()	
+});
+
+// const onlineClickHndl = (streamData) =>{
+// 	$("#online-ch").on('click', () => {
+// 		const hide = (streamData) =>{
+// 			// for(var i = 0; i < users.length; i++){
+// 				console.log(streamData);
+// 				if (streamData.stream == null){
+// 					$("#result-box").css('display', 'none')
+// 				}
+// 			// }
+// 		}
+// 		clearAll();
+// 		fetchAll(hide(streamData));
+// 	})
+// }
